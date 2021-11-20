fed385c2cf35f9a5cc08cc0c1e3d5c1d5454e1b1
R. Fadhil <ridhanfadhilah@gmail.com>
2021-11-20 13:19:02 +0700

    array and tuples

---
+++ src/ts03_array_tuples/index.ts
@@ -0,0 +1,17 @@
+// array or list
+let prime: number[] = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+let friends: Array<string> = ["Fathur", "Khansa", "Faisal", "Christ",]
+let arr: any[] = [1, true, 'Hello']
+
+// Tuple
+let biodata: [string, number] = ["age", 24]
+let person: [number, string, boolean] = [1, 'brad', true]
+
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill'],
+]
